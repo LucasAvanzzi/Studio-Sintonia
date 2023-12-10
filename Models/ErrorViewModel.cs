43186656bb19ae159d877e92d0ac50fdaac02cf7
LucasAvanzzi <105827861+LucasAvanzzi@users.noreply.github.com>
2023-12-10 20:15:40 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Studio_Sintonia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
